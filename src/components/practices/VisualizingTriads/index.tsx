e95b1ab7428cb1e44a1d0d8c358817a00e86476e
Nate Kane <74371715+Nate-Kane@users.noreply.github.com>
2026-05-28 16:31:05 -0600

    Scaffold Visualizing Triads practice

---
+++ src/components/practices/VisualizingTriads/index.tsx
@@ -0,0 +1,37 @@
+import { FC } from 'react';
+import { useKeyGenerator } from '../sharedPracticeComponents/hooks/useKeyGenerator';
+import KeyDisplay from '../sharedPracticeComponents/KeyDisplay';
+import TriadsDisplay from '../sharedPracticeComponents/TriadsDisplay';
+
+interface VisualizingTriadsProps {
+  skillLevel: string;
+}
+
+const VisualizingTriads: FC<VisualizingTriadsProps> = ({ skillLevel }) => {
+  const {
+    currentKey,
+    generateNewKey,
+    selectKey,
+    isKeySelectorOpen,
+    openKeySelector,
+    closeKeySelector,
+  } = useKeyGenerator(skillLevel);
+
+  return (
+    <div className="space-y-6">
+      <KeyDisplay
+        currentKey={currentKey}
+        onRegenerateKey={generateNewKey}
+        skillLevel={skillLevel}
+        isKeySelectorOpen={isKeySelectorOpen}
+        openKeySelector={openKeySelector}
+        closeKeySelector={closeKeySelector}
+        onSelectKey={selectKey}
+      />
+
+      {currentKey && <TriadsDisplay currentKey={currentKey} />}
+    </div>
+  );
+};
+
+export default VisualizingTriads;
