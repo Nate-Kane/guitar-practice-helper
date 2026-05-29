50e08cf784c8ea1a2e0db92f8e4d1ed8fc643d19
Nate Kane <74371715+Nate-Kane@users.noreply.github.com>
2026-05-28 19:39:42 -0600

    Update directions logo & fretboard pic

--- src/components/practices/sharedPracticeComponents/DirectionsSection.tsx
+++ src/components/practices/sharedPracticeComponents/DirectionsSection.tsx
@@ -20,9 +20,8 @@ const DirectionsSection: FC<DirectionsSectionProps> = ({ directions }) => (
         className="h-5 w-5 mr-2 shrink-0"
         aria-hidden="true"
       >
-        <circle cx="12" cy="12" r="10"></circle>
-        <path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3"></path>
-        <path d="M12 17h.01"></path>
+        <path d="M12 7v14" />
+        <path d="M3 18a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h5a4 4 0 0 1 4 4 4 4 0 0 1 4-4h5a1 1 0 0 1 1 1v13a1 1 0 0 1-1 1h-6a3 3 0 0 0-3 3 3 3 0 0 0-3-3z" />
       </svg>
       Directions
     </h3>
